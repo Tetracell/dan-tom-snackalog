96b9948a93f064d59dd5e4f66acd63af1fd4751a
Tom LaTulipe <tomlatulipe@pursuit.org>
2022-08-16 20:52:55 -0400

    basics of app backend filled

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,27 @@
 // DEPENDENCIES
 
 const express = require("express");
+const cors = require("cors");
 
 // CONFIGURATION
 const app = express();
 
 // MIDDLEWARE
+app.use(cors());
+app.use(express.json());
 
 // ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to Snack-a-log");
+});
+
+const snackController = require("./controllers/snackController");
+app.use("/snacks", snackController);
+
+//404
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
 
 // EXPORT
 module.exports = app;
