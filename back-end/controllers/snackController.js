118b61de97fd25c1de882101680cc7fb2bca3c2d
Tom LaTulipe <tomlatulipe@pursuit.org>
2022-08-17 16:24:34 -0400

    Added additional info tests were looking for

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -4,25 +4,32 @@ const snacks = express.Router();
 
 const { getAllSnacks, getSnack } = require("../queries/snacks");
 
-//Index
+//Index - All snacks
 snacks.get("/", async (req, res) => {
   const allSnacks = await getAllSnacks();
   if (allSnacks[0]) {
     // A check to make sure there is at least one snack
-    res.status(200).json(allSnacks);
+    res.status(200).json({ payload: allSnacks, success: true });
   } else {
-    res.status(500).json({ error: "Server Error - no snacks found?" });
+    res.status(500).json({
+      success: false,
+      error: "Server Error - no snacks found?",
+    });
   }
 });
 
 //Routes
+
+//Single Snack
 snacks.get("/:id", async (req, res) => {
   const { id } = req.params;
   const snack = await getSnack(id);
   if (snack.name !== "QueryResultError") {
-    res.json(snack);
+    res.json({ payload: snack, success: true });
   } else {
-    res.status(404).json({ error: "Snack not found" });
+    res
+      .status(404)
+      .json({ payload: "not found", success: false, error: "Snack not found" });
   }
 });
 
